93b6bd00aebb14c5743bf6bf1e13001b11d95703
IgorCamps <igoruser07@gmail.com>
2025-09-18 16:45:46 -0300

    Cadastro usuários

--- server.js
+++ server.js
@@ -9,5 +9,3 @@ app.use('/', publicRoutes);
 
 
 app.listen(3000, () => console.log("Servidor Rodando 🚀"));
-
-// mongodb+srv://igoruser07_db_user:KlAPisxU62IWS584@cluster0.4bfdu7z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
